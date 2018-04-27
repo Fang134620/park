126247b2f83af4684a2af6e76b740aa77fdecba6
Fang134620 <fanghao1346@163.com>
2018-04-27 16:33:38 +0800

    0.5

--- routes/index.js
+++ routes/index.js
@@ -26,14 +26,11 @@ router.post('/register',function(req,res,next){
   })
 })
 
-router.get('/login', function (req, res) {
-  res.render('login')
-})
-
 /*登录*/
 router.post('/login',function(req,res,next){
   var userName = req.body.userName;
   var password = req.body.password;
+  console.log(userName,password);
   var hash = crypto.createHash('md5');
   hash.update(password);
   password = hash.digest('hex');
@@ -43,7 +40,7 @@ router.post('/login',function(req,res,next){
       req.session.userID = rows[0].userID;
       req.session.userName = userName;
       req.session.nickname = rows[0].nickname;
-      res.json({success:1})
+      res.json({success:1,message:null});
       console.log(req.session)
     }else{
       res.json({success:0,message:'用户名或密码错误'});
@@ -66,7 +63,7 @@ router.get('/',function(req,res,next){
     login = true;
     var lastTime = req.query.lastTime;
     var userID = req.session.userID;
-    var query2 = 'select count(*) from message where receiveID = '+userID+' and sendTime>'+lastTime;
+    var query2 = 'select count(*) from message where receiveID = '+userID;
     var query3 = 'select count(*) from notice where noticeTime>'+lastTime;
     async function funs(){
       docs = [query1,query2,query3];
@@ -79,7 +76,7 @@ router.get('/',function(req,res,next){
       })
       let results = await Promise.all(promises);
       var number = results[1][0]["count(*)"]+results[2][0]["count(*)"];
-      res.json({success:1,login:true,shares:results[0],newMsgMum:number})
+      res.json({success:1,login:true,shares:results[0],newMsgNum:number})
     }
     funs();
   }else{
@@ -131,21 +128,6 @@ router.get('/message/:messageID',function(req,res,next){
   })
 })
 
-/*加入黑名单*/
-router.post('/message/:messageID/pullInBlack',function(req,res,next){
-  var messageID = req.params.messageID;
-  var userID = req.session.userID;
-  var query1 = 'select senderID,senderName from message where messageID = '+messageID;
-  var query2 = 'insert into blacklist set userID ='+userID+',pulledID ='+pulledID+',pulledName ='+pulledName;
-  db.query(query1,function(err,rows,fields){
-    var pulledID = rows[0].senderID;
-    var pulledName = rows[0].senderName;
-    db.query(query2,function(err,rows,fields){
-      res.json({success:1});
-    })
-  })
-})
-
 /*公告详情*/
 router.get('/notice/:noticeID',function(req,res,next){
   var noticeID = req.params.noticeID;
@@ -164,86 +146,27 @@ router.post('/message/:messageID/delete',function(req,res,next){
   })
 })
 
-/*发送消息*/
-router.post('/sendMsg',function(req,res,next){
-  var receiveID = req.query.receiveID;
-  var senderID = req.session.userID;
-  var senderName = req.session.userName;
-  var msgTitle = req.body.msgTitle;
-  var msgContent = req.body.msgContent;
-  var query1 = 'select blacklistID from blacklist where userID = '+receiveID+' and pulledID ='+senderID;
-  var query2 = 'insert into message set receiveID = '+receiveID+',senderID = '+senderID+',senderName = \"'+senderName+'\",msgTitle = \"'+msgTitle+'\",msgContent = \"'+msgContent+'\",msgType = 1,sendTime = NOW()';
-  async function ifBlack(){
-    db.query(query1,function(err,rows,fields){
-      return new Promise(function(resolve,reject){
-        if(err)console.log(err);
-        resolve(rows[0]);
-      }) 
-    })
-  }
-  async function sendMsg(){
-    db.query(query2,function(err,rows,fields){
-      return new Promise(function(resolve,reject){
-        if(err)console.log(err);
-        resolve();
-      })
-    })
-  }
-  async function funs(){
-    var doIfBlack = await ifBlack();
-    if(doIfBlack){
-      res.json({success:0,message:"发送失败，已被对方加入黑名单"});
-    }else{
-      var doSendMsg = await sendMsg();
-      res.json({success:1});
-    }
-  }
-  funs();
-})
-
 /*发布车位*/
 router.post('/createShare',function(req,res,next){
-  var renderID = req.session.userID
-  var renderName = req.session.nickname;
+  var renderID = req.session.userID;
   var position = req.body.position;
   var beginTime = req.body.beginTime;
   var endTime = req.body.endTime;
   var cost = req.body.cost;
   
-  function getCredit(){
-    return new Promise(function(resolve,reject){
-      var query1 = 'select credit from user where userID = '+ renderID;
-      db.query(query1,function(err,rows,fields){
-        resolve(rows[0]);
-      })
-    }) 
-  }
-  function createShare(renderCredit){
-    return new Promise(function(resolve,reject){
-      var query2 = 'insert into share set renderID = '+renderID+',renderName = \"'+renderName+'\",renderCredit = '+renderCredit+',position = \"'+position+'\",beginTime =\"'+beginTime+'\",endTime =\"'+endTime+'\",state = 0,cost = '+cost;
-      db.query(query2,function(err,rows,fields){
-        if(err) console.log(err);
-        resolve();
-      })
-    })
-    
-  }
-  async function funs(){
-    var credit = await getCredit();
-    var renderCredit = credit.credit;
-    var resulet = await createShare(renderCredit);
+  var query2 = 'insert into share set renderID = '+renderID+',position = \"'+position+'\",beginTime =\"'+beginTime+'\",endTime =\"'+endTime+'\",state = 0,cost = '+cost;
+  db.query(query2,function(err,rows,fields){
     res.json({success:1});
-  }
-  funs();
+  })
 })
 
 /*车位详情*/
 router.get('/share/:shareID',function(req,res,next){
   var shareID = req.params.shareID;
   var userID = req.session.userID;
-  var query1 = 'select * from share where shareID ='+ shareID;
+  var query1 = 'select position,beginTime,endTime,state,cost,renderID,borrowerID,nickname as renderName,credit from share inner join user on share.renderID = user.userID where shareID ='+ shareID;
   db.query(query1,function(err,rows,fields){
-    res.json({success:1,share:rows[0],userID:userID});
+    res.json({success:1,share:rows[0]});
   })
 })
 
@@ -254,7 +177,7 @@ router.post('/share/:shareID/accept',function(req,res,next){
     var borrowerID = req.session.userID;
     var query1 = 'select balance from user where userID ='+borrowerID;
     var query2 = 'select renderID,cost,position from share where shareID = '+shareID;
-    async function getBalance(){
+    function getBalance(){
       return new Promise(function(resolve,reject){
         db.query(query1,function(err,rows,fields){
           console.log(rows);
@@ -262,7 +185,7 @@ router.post('/share/:shareID/accept',function(req,res,next){
         })
       })
     }
-    async function getShare(){
+    function getShare(){
       return new Promise(function(resolve,reject){
         db.query(query2,function(err,rows,fields){
           console.log(rows);
@@ -280,7 +203,7 @@ router.post('/share/:shareID/accept',function(req,res,next){
         var msgContent = "您位于"+share.position+"的车位已被租用";
         var query3 = 'update share set borrowerID = '+borrowerID+',state = 1 where shareID ='+shareID;
         var query4 = 'update user set balance = balance-'+cost+' where userID = '+borrowerID;
-        var query5 = 'insert into message(receiveID,msgTitle,msgContent,msgType,sendTime) values('+renderID+',\"系统通知\",\"'+msgContent+'\",0,NOW())';
+        var query5 = 'insert into message(receiveID,msgTitle,msgContent,sendTime) values('+renderID+',\"系统通知\",\"'+msgContent+'\",NOW())';
         let docs = [query3,query4,query5];
         let promises = docs.map((doc)=>{
           return new Promise(function(resolve,reject){
@@ -329,7 +252,7 @@ router.post('/share/:shareID/windup',function(req,res,next){
     var msgContent = "您的车单已被结束租用，最终评分为："+stars+"星";
     var query2 = 'update user set balance = balance +'+cost+',credit = credit*('+(0.4+stars/5)+') where userID = '+renderID;
     var query3 = 'update share set state =2,stars = '+stars+' where shareID = '+shareID;
-    var query4 = 'insert into message(receiveID,msgTitle,msgContent,msgType,sendTime) values('+renderID+',\"系统通知\",\"'+msgContent+'\",0,NOW())';
+    var query4 = 'insert into message(receiveID,msgTitle,msgContent,sendTime) values('+renderID+',\"系统通知\",\"'+msgContent+'\",NOW())';
     let docs = [query2,query3,query4];
     let promises = docs.map((doc)=>{
       return new Promise(function(resolve,reject){
@@ -393,26 +316,9 @@ router.post('/userCenter/modify',function(req,res,next){
   })
 })
 
-/*查看黑名单*/
-router.get('/userCenter/blacklist',function(req,res,next){
-  var userID = req.session.userID;
-  var query = 'select * blacklist where userID ='+userID;
-  db.query(query,function(err,rows,fields){
-    res.json({success:1,blacklists:rows});
-  })
-})
-
-/*修改黑名单*/
-router.post('/userCenter/blacklist/delete',function(req,res,next){
-  var blacklistIDs = req.body.blacklistIDs;
-  var query = 'delete from blacklist where blacklistID in '+blacklistIDs;
-  db.query(query,function(err,rows,fields){
-    res.json({success:1});
-  })
-})
-
 /*管理主页*/
 router.get('/admin',function(req,res,next){
+  if(!req.session.adminLogin) res.redirect('/adminLogin')
   var query = 'select appealID,userID,apContent from appeal where apstate = false';
   db.query(query,function(err,rows,fields){
     if(err)console.log(err);
@@ -422,14 +328,13 @@ router.get('/admin',function(req,res,next){
 
 /*申诉详情*/
 router.get('/appeal/:appealID',function(req,res,next){
+  if(!req.session.adminLogin) res.redirect('/adminLogin')
   var appealID = req.params.appealID;
   var query1 = 'select * from appeal where appealID ='+appealID;
   db.query(query1,function(err,rows,fields){
     var appeal = rows[0];
     var query2 = 'select * from share where shareID = '+appeal.shareID;
     db.query(query2,function(err,rows,fields){
-      console.log(appeal)
-      console.log(rows[0])
       res.render('appeal',{success:1,appeal:appeal,share:rows[0]})
     })
   })
@@ -437,6 +342,7 @@ router.get('/appeal/:appealID',function(req,res,next){
 
 /*申诉处理*/
 router.post('/appeal/:appealID',function(req,res,next){
+  if(!req.session.adminLogin) res.redirect('/adminLogin')
   var apResult = req.query.apResult;
   var appealID = req.params.appealID;
   var shareID = req.query.shareID;
@@ -447,8 +353,8 @@ router.post('/appeal/:appealID',function(req,res,next){
     var query3 = 'update share set state = 3 where shareID ='+shareID;
     var query4 = 'update user set balance = balance - '+cost+',credit = credit*0.5 where userID = '+renderID;
     var query5 = 'update user set balance = balance + '+cost+' where userID = '+borrowerID;
-    var query6 = 'insert into message(receiveID,msgTitle,msgContent,msgType,sendTime) values('+renderID+',\"系统通知\",'+renderMsgContent+',0,NOW())';
-    var query7 = 'insert into message(receiveID,msgTitle,msgContent,msgType,sendTime) values('+borrowerID+',\"系统通知\",'+borrowerMsgContent+',0,NOW())';
+    var query6 = 'insert into message(receiveID,msgTitle,msgContent,sendTime) values('+renderID+',\"系统通知\",'+renderMsgContent+',NOW())';
+    var query7 = 'insert into message(receiveID,msgTitle,msgContent,sendTime) values('+borrowerID+',\"系统通知\",'+borrowerMsgContent+',NOW())';
     var query8 = 'update appeal set apResult = true,apState = true where appealID = '+appealID;
 
     function getShare(){
@@ -485,7 +391,7 @@ router.post('/appeal/:appealID',function(req,res,next){
     async function funs(){
       var msgContent = "您好，您的申诉被驳回";
       var query1 = 'update appeal set apState = true,apResult = false where appealID = '+appealID;
-      var query2 = 'insert into message(receiveID,msgTitle,msgContent,msgType,sendTime) values('+borrowerID+',\"系统通知\",\"'+msgContent+'\",0,NOW())';
+      var query2 = 'insert into message(receiveID,msgTitle,msgContent,sendTime) values('+borrowerID+',\"系统通知\",\"'+msgContent+'\",NOW())';
       let docs = [query1,query2];
       let promises = docs.map((doc)=>{
         return new Promise(function(resolve,reject){
@@ -502,18 +408,112 @@ router.post('/appeal/:appealID',function(req,res,next){
   }
 })
 
+/*管理员身份验证*/
+router.get('/adminLogin',function(req,res,next){
+  res.render('adminLogin',{message:""});
+})
+
+router.post('/doAdminLogin',function(req,res,next){
+  var adminName = req.body.adminName;
+  var password = req.body.password;
+  var hash = crypto.createHash('md5');
+  hash.update(password);
+  password = hash.digest('hex');
+  var query = 'select adminID from admin where adminName = '+mysql.escape(adminName)+' and password = '+mysql.escape(password);
+  db.query(query,function(err,rows,fields){
+    if(err)console.log(err);
+    if(rows[0]){
+      req.session.adminLogin = true;
+      res.redirect('/admin')
+      return;
+    }else{
+      req.session.adminLogin = false;
+      res.render('adminLogin',{message:"帐号或密码错误，请重试"})
+    }
+  })
+})
+
+/*查看系统情况*/
+router.get('/situation',function(req,res,next){
+  if(!req.session.adminLogin) res.redirect('/adminLogin')
+  var query1 = 'select count(*) from user';//总用户数
+  var query2 = 'select count(*) from share where state = 2';//已完成车单数目
+  var query3 = 'select count(*) from share where state = 0';//当前系统发布的可用车单数
+  var query4 = 'select sum(cost) from share where state = 2';//已完成的交易总额
+  var query5 = 'SELECT sum(cost) FROM share WHERE DATE_FORMAT( beginTime, \'%Y%m\') = DATE_FORMAT(now(),\'%Y%M\') and state = 2';//本月完成的交易总额
+  var query6 = 'select userID,nickname,balance,credit from user where credit < 60';
+  
+  async function funs(){
+    var docs = [query1,query2,query3,query4,query5,query6];
+    let promises = docs.map((doc)=> {
+      return new Promise(function(resolve,reject){
+        db.query(doc,function(err,rows,fields){
+          if(err)console.log(err);
+          resolve(rows);
+        })
+      })
+    })
+    var resulets = await Promise.all(promises);
+    console.log(resulets);
+    res.render('situation',{totalUserNum:resulets[0][0]["count(*)"],endShareNum:resulets[1][0]["count(*)"],avaiShareNum:resulets[2][0]["count(*)"],totalCost:resulets[3][0]["sum(cost)"],monthCost:resulets[4][0]["sum(cost)"],badusers:resulets[5]});
+  }
+  funs();
+})
+
+/*查看用户交易记录*/
+router.get('/baduser/:userID',function(req,res,next){
+  if(!req.session.adminLogin) res.redirect('/adminLogin')
+  var userID = req.params.userID;
+  var query1 = 'select * from share where renderID = '+userID+' order by state';
+  var query2 = 'select count(*) from share where renderID = '+userID;
+  var query3 = 'select count(*) from share where borrowerID = '+userID;
+  async function funs(){
+    let docs = [query1,query2,query3];
+    let promises = docs.map((doc)=>{
+      return new Promise(function(resolve,reject){
+        db.query(query,function(err,rows,fields){
+          if(err) console.log(err);
+          resolve(rows);
+        })
+      })
+    })
+    var resulets = await Promise.all(promises);
+    res.render('baduser',{shares:resulets[0],rendNum:resulets[1][0]["count(*)"],borrowNum:resulets[1][0]["count(*)"]});
+  }
+  
+})
+
+/*封停用户*/
+router.post('/baduser/:userID/delete',function(req,res,next){
+  if(!req.session.adminLogin) res.redirect('/adminLogin')
+  var userID = req.params.userID;
+  var query = 'delete from user where userID = '+userID;
+  db.query(query,function(err,rows,fields){
+    res.redirect('/situation');
+  })
+})
+
 /*发布公告*/
 router.get('/CNotice',function(req,res,next){
-  res.render('createNotice');
+  if(req.session.adminLogin){
+    res.render('createNotice');
+  }else{
+    res.redirect('/adminLogin');
+  }
 })
 
 router.post('/createNotice',function(req,res,next){
-  var noticeTitle = req.body.noticeTitle;
-  var noticeContent = req.body.noticeContent;
-  var query = 'insert into notice(noticeTitle,noticeContent,noticeTime) values(\"'+noticeTitle+'\",\"'+noticeContent+'\",NOW())';
-  db.query(query,function(err,rows,fields){
-    res.redirect('/admin');
-  })
+  if(req.session.adminLogin){
+    var noticeTitle = req.body.noticeTitle;
+    var noticeContent = req.body.noticeContent;
+    var query = 'insert into notice(noticeTitle,noticeContent,noticeTime) values(\"'+noticeTitle+'\",\"'+noticeContent+'\",NOW())';
+    db.query(query,function(err,rows,fields){
+      res.redirect('/admin');
+    })
+  }else{
+    res.redirect('/adminLogin');
+  }
+  
 })
 
 module.exports = router;
